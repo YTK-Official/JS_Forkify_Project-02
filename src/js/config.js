6e55e37e9c73fe4c9831e4d7c921c0c7609cfd34
Youssef Tarek <101991889+YTK-Official@users.noreply.github.com>
2024-10-01 15:13:11 +0300

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'
+export const TIMEOUT_SEC = 10
+export const RES_PER_PAGE = 10
+export const KEY = '5729515b-fa1b-4ac0-95cf-d49e49a049a5'
+export const MODAL_CLOSE_SEC = 2.5
+
+// https://forkify-api.herokuapp.com/v2
+
+// e1905430-e708-45c5-ba79-1d0e506d65c4
